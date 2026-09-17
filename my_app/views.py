b015f6be449480966e263096ba60aa3627e92eab
Anatoliy Nakonechnyy <anatoliy.nakonechnyy@gmail.com>
2026-09-17 23:41:29 +0300

    commit6

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from .models import Book, Category
@@ -5,12 +6,21 @@ from .models import Book, Category
 
 def home_page(request):
 
-    categories = Category.objects.all()
+    search_query = request.GET.get("search", "")
+
     books = Book.objects.select_related("category").all()
+    categories = Category.objects.all()
+
+    if search_query:
+        books = books.filter(
+            Q(title__icontains=search_query)
+            | Q(author__icontains=search_query)
+            | Q(category__name__icontains=search_query)
+        )
 
     context = {
         "books": books,
         "categories": categories,
+        "search_query": search_query,
     }
-
     return render(request, "my_app/home_page.html", context)
