afbd7545e0656e9d0486be4c0a3b879555796ee6
Anatoliy Nakonechnyy <anatoliy.nakonechnyy@gmail.com>
2026-09-17 23:14:34 +0300

    commit 4

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,21 +1,32 @@
+from typing import ClassVar
+
 from django.contrib import admin
 
 from .models import Book, Category
 
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "price")
-
-    list_display_links = ("title",)
-
-    search_fields = ("title", "description")
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 1
+    fields = (
+        "title",
+        "author",
+        "price",
+    )
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "slug")
-
     list_display_links = ("name",)
-
     search_fields = ("name",)
+    inlines: ClassVar[list] = [BookInline]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "price")
+    list_display_links = ("title",)
+    search_fields = ("title", "description")
+    list_editable = ("price",)
+    list_filter = ("category",)
--- my_app/views.py
+++ my_app/views.py
@@ -1,28 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Book, Category
 
 
 def home_page(request):
 
-    books = Book.objects.all()
     categories = Category.objects.all()
+    books = Book.objects.select_related("category").all()
 
-    html_content = "<h1>List of Categories</h1><ul>"
-    if not categories:
-        html_content += "<li>No categories available. Add them in the admin panel!</li>"
-    else:
-        for category in categories:
-            html_content += f"<li><strong>{category.name}</strong></li>"
-    html_content += "</ul>"
+    context = {
+        "books": books,
+        "categories": categories,
+    }
 
-    html_content += "<h1>List of Books</h1><ul>"
-    if not books:
-        html_content += "<li>No books available. Add them in the admin panel!</li>"
-    else:
-        for book in books:
-            html_content += f"<li><strong>{book.title}</strong> — {book.price} usd. - {book.category.name}</li>"
-
-    html_content += "</ul>"
-
-    return HttpResponse(html_content)
+    return render(request, "my_app/home_page.html", context)
